b5ef0146d4a20a2fae3705e6de61823105fbf1e0
Artyom.Semashko <simkarik@gmail.com>
2022-12-23 06:24:32 +0300

    Solved 5_1

--- HW_5_1/Program.cs
+++ HW_5_1/Program.cs
@@ -8,3 +8,42 @@ void FillMass(int[] mas)
         i++;
     }
 }
+
+void PrintMass(int[] mass)
+{
+    int pos = 0;
+    while (pos< mass.Length -1)
+    {
+        Console.Write($"{mass[pos]}, ");
+        pos++;
+    }
+    Console.Write(mass[pos]);
+    Console.WriteLine();
+}
+
+int EvenCount(int[] arr)
+{
+    int i = 0;
+    int sum = 0;
+    while (i < arr.Length)
+    {
+        if(arr[i]%2 == 0)
+        {
+            sum++;
+        }
+        i++;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int s = int.Parse(Console.ReadLine());
+
+int[] array = new int[s];
+
+Console.WriteLine("Сгенерированный массив: ");
+FillMass(array);
+PrintMass(array);
+
+int result = EvenCount(array);
+Console.WriteLine($"Количество чётных чисел в массиве = {result}");
