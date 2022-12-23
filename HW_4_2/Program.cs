8de37afbe18ef9e3b93c68fd1da2d405aa8c8e7b
Artyom.Semashko <simkarik@gmail.com>
2022-12-23 05:51:45 +0300

    Solved 4_2

---
+++ HW_4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: "); 
+int num = int.Parse(Console.ReadLine());
+
+int NumSum(int n)
+{
+int sum = 0;
+    while(n >= 1)
+    {
+        sum += n%10;
+         n = n/10;
+    }
+return sum;
+}
+    int s = NumSum(num);
+    Console.WriteLine($"Сумма цифр в числе - {s}");
