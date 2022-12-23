1983b7133ed559fc067913b5fd69b0435bf25b5a
Artyom.Semashko <simkarik@gmail.com>
2022-12-23 06:05:31 +0300

    Solved 4_3

---
+++ HW_4_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (На самом деле просто тест на внимательность к лекциям:) )
+void FillMass(int[] mas)
+{
+    int i = 0;
+    while (i < mas.Length)
+    {
+        mas[i] = new Random().Next(1, 100);
+        i++;
+    }
+}
+
+void PrintMass(int[] mass)
+{
+    int pos = 0;
+    while (pos< mass.Length -1)
+    {
+        Console.Write($"{mass[pos]}, ");
+        pos++;
+    }
+    Console.Write(mass[pos]);
+}
+
+int[] array = new int[8];
+
+FillMass(array);
+PrintMass(array);
